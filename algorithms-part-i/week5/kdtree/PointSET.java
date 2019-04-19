3d44c6589e1d40ba918c964b811e238249c0b2d3
Ricardo Pacheco <rjgpacheco@gmail.com>
2019-04-19 19:32:00 +0100

    Week 5 - first submission

--- algorithms-part-i/week5/kdtree/PointSET.java
+++ algorithms-part-i/week5/kdtree/PointSET.java
@@ -8,6 +8,10 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class PointSET {
 
     private SET<Point2D> pointSet;
@@ -29,11 +33,15 @@ public class PointSET {
 
     public void insert(Point2D p) {
         // add the point to the set (if it is not already in the set)
+        if (p == null) throw new IllegalArgumentException();
+
         pointSet.add(p);
     }
 
     public boolean contains(Point2D p) {
         // does the set contain point p?
+        if (p == null) throw new IllegalArgumentException();
+
         return pointSet.contains(p);
     }
 
@@ -44,12 +52,54 @@ public class PointSET {
 
     public Iterable<Point2D> range(RectHV rect) {
         // all points that are inside the rectangle (or on the boundary)
-        return null;
+        if (rect == null) throw new IllegalArgumentException();
+
+        if (pointSet.isEmpty()) {
+            return null;
+        }
+
+        List<Point2D> points = new ArrayList<Point2D>();
+
+        Iterator<Point2D> allPoints = pointSet.iterator();
+        while (allPoints.hasNext()) {
+            Point2D point = allPoints.next();
+            if (rect.contains(point)) {
+                points.add(point);
+            }
+        }
+        return points;
     }
 
     public Point2D nearest(Point2D p) {
         // a nearest neighbor in the set to point p; null if the set is empty
-        return null;
+        // Brute force approach, as askek
+
+        if (p == null) throw new IllegalArgumentException();
+        
+        if (pointSet.isEmpty() || pointSet.size() == 1) {
+            return null;
+        }
+
+        Point2D closest = null;
+        double closestDistance = Double.POSITIVE_INFINITY;
+
+        Iterator<Point2D> allPoints = pointSet.iterator();
+
+        while (allPoints.hasNext()) {
+            Point2D point = allPoints.next();
+            if (point.equals(p)) {
+                continue;
+            }
+
+            double distance = p.distanceTo(point);
+
+            if (distance < closestDistance) {
+                closest = point;
+                closestDistance = distance;
+            }
+        }
+
+        return closest;
     }
 
     public static void main(String[] args) {
